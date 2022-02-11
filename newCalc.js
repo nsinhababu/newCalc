648319c4f8aa43a66418c20cf156d330d16e24d9
nilanjan <nsinhababu@outlook.com>
2022-02-11 17:13:53 +0530

    redesigned the app

--- newCalc.js
+++ newCalc.js
@@ -1,49 +1,58 @@
-const numberBtn = document.querySelectorAll('.number-btn');
-const signBtn = document.querySelectorAll('.sign-btn');
+const resultScreen = document.querySelector('.result');
+
+const allResetBtn = document.querySelector('.reset-btn');
+const delBtn = document.querySelector('.del-btn');
+
+const numberBtns = document.querySelectorAll('.number-btn');
+const signBtns = document.querySelectorAll('.sign-btn');
+
+const pointBtn = document.querySelector('.point-btn');
+
 const equalBtn = document.querySelector('.equal-btn');
-const resetBtn = document.querySelector('.reset-btn');
-const backBtn = document.querySelector('.back-btn');
-const resultDiv = document.querySelector('.result');
-let equation;
 
-console.log(resetBtn);
+let isSign = false;
+let isPoint = false;
 
-// for (let i = 0; i < numberBtn.length; i++) {
-//   numberBtn[i].value = numberBtn[i].innerText;
-//   numberBtn[i].addEventListener('click', () => {
-//     resultDiv.innerText += numberBtn[i].value;
-//   });
-//   console.log();
-// }
-
-function masterFunction(param) {
-  for (let i = 0; i < param.length; i++) {
-    param[i].value = param[i].innerText;
-    param[i].addEventListener('click', () => {
-      resultDiv.innerText += param[i].value;
-    });
-    // console.log(param[i].value);
-  }
+for (let i = 0; i < numberBtns.length; i++) {
+  numberBtns[i].addEventListener('click', (e) => {
+    resultScreen.innerText += e.target.innerText;
+    // isPoint = false;
+    isSign = false;
+  });
 }
-masterFunction(numberBtn);
-masterFunction(signBtn);
+for (let i = 0; i < signBtns.length; i++) {
+  signBtns[i].addEventListener('click', (e) => {
+    if (!isSign) {
+      resultScreen.innerText += e.target.innerText;
+      isSign = true;
+      isPoint = false;
+    }
+  });
+}
+
+pointBtn.addEventListener('click', () => {
+  if (!isPoint) {
+    resultScreen.innerText += pointBtn.innerText;
+    console.log('hi');
+    isPoint = true;
+  }
+});
+
+delBtn.addEventListener('click', () => {
+  const slicedExpression = resultScreen.innerText.slice(0, -1);
+  resultScreen.innerText = slicedExpression;
+  isPoint = false;
+  isSign = false;
+});
+
+allResetBtn.addEventListener('click', () => {
+  resultScreen.innerText = '';
+});
 
 equalBtn.addEventListener('click', () => {
-  equation = resultDiv.innerText;
-  resultDiv.innerText = eval(equation);
-});
-
-resetBtn.addEventListener('click', () => {
-  resultDiv.innerText = '';
-});
-// backBtn.addEventListener('click', () => {
-//   const stringArr = resultDiv.innerText.split('');
-//   stringArr.pop();
-//   resultDiv.innerText = stringArr.join('');
-// });
-
-backBtn.addEventListener('click', () => {
-  let text = resultDiv.innerText;
-  let slicedText = text.slice(0, -1);
-  resultDiv.innerText = slicedText;
+  if (resultScreen.innerText !== '') {
+    resultScreen.innerText = eval(resultScreen.innerText);
+    isPoint = false;
+    isSign = false;
+  }
 });
